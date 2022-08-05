9e300f555e7ebd85fc454fa57988b59a2b9548b9
GarryAmasia <garryamasia@outlook.com>
2022-08-05 18:27:01 +1000

    contact section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import { NavBar } from "./components/NavBar";
 import Skills from "./components/Skills";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Skills />
       <Works />
+      <Contact />
     </div>
   );
 }
